453ba52cf8dd7a732068261b4dd580897ca5434c
Andrew Jackson <andrew@codechimp.org>
2025-05-21 16:57:04 +0100

    Improve get entities for label

--- custom_components/label_state/binary_sensor.py
+++ custom_components/label_state/binary_sensor.py
@@ -196,22 +196,23 @@ class LabelStateBinarySensor(BinarySensorEntity):
 
         await super().async_added_to_hass()
 
-        registry = er.async_get(self.hass)
-        for entity in registry.entities.values():
-            for label in entity.labels:
+        ent_reg = er.async_get(self.hass)
+        entries = er.async_entries_for_label(ent_reg, self._label)
+        for entry in entries:
+            for label in entry.labels:
                 if label == self._label:
                     LOGGER.debug(
                         "Found label %s in entity %s",
                         self._label,
-                        entity.entity_id,
+                        entry.entity_id,
                     )
 
                     # Replay current state of the labelled entitiy
-                    state = self.hass.states.get(entity.entity_id)
+                    state = self.hass.states.get(entry.entity_id)
                     state_event: Event[EventStateChangedData] = Event(
                         "",
                         {
-                            "entity_id": entity.entity_id,
+                            "entity_id": entry.entity_id,
                             "new_state": state,
                             "old_state": None,
                         },
@@ -221,7 +222,7 @@ class LabelStateBinarySensor(BinarySensorEntity):
 
                     self.async_on_remove(
                         async_track_state_change_event(
-                            self.hass, entity.entity_id, self._async_state_listener
+                            self.hass, entry.entity_id, self._async_state_listener
                         )
                     )
 
