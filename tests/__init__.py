91a60864f4e1c8d36ea28e01981323494aa226cb
Andrew Jackson <andrew@codechimp.org>
2025-05-27 14:44:55 +0100

    Tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,3 +3,14 @@
 import pytest
 
 pytestmark = pytest.mark.asyncio
+
+from homeassistant.core import HomeAssistant
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+
+async def setup_integration(hass: HomeAssistant, config_entry: MockConfigEntry) -> None:
+    """Fixture for setting up the component."""
+    config_entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(config_entry.entry_id)
+    await hass.async_block_till_done()
--- tests/test_binary_sensor.py
+++ tests/test_binary_sensor.py
@@ -1,9 +1,16 @@
 """The test for the label_state binary sensor platform."""
 
+import pytest
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers import label_registry as lr
 from homeassistant.setup import async_setup_component
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 
+from . import setup_integration
+
+VALUES_STATE1 = ["on", "off", "unavailable"]
+VALUES_STATE2 = ["on", "off", "unavailable"]
 VALUES_NUMERIC = [17, 20, 15.2, 5, 3.8, 9.2, 6.7, 14, 6]
 VALUES_ERROR = [17, "string", 15.3]
 COUNT = len(VALUES_NUMERIC)
@@ -12,104 +19,145 @@ MAX_VALUE = max(VALUES_NUMERIC)
 LAST_VALUE = VALUES_NUMERIC[-1]
 
 
-# async def test_min_sensor(
-#     hass: HomeAssistant, entity_registry: er.EntityRegistry
-# ) -> None:
-#     """Test the min sensor."""
-#     sensor_entity_entry = entity_registry.async_get_or_create(
-#         "sensor", "test_1", "unique", suggested_object_id="test_1"
-#     )
-#     assert sensor_entity_entry.entity_id == "sensor.test_1"
+@pytest.mark.parametrize(
+    (
+        "state_1",
+        "state_2",
+        "expected_state",
+    ),
+    [
+        (
+            "unavailable",
+            "on",
+            "on",
+        ),
+        (
+            "on",
+            "on",
+            "off",
+        ),
+    ],
+)
+async def test_state_sensor(
+    hass: HomeAssistant,
+    state_1: str,
+    state_2: str,
+    expected_state: str,
+    entity_registry: er.EntityRegistry,
+    label_registry: lr.LabelRegistry,
+) -> None:
+    """Test the state sensor."""
 
-#     config = {
-#         "sensor": {
-#             "platform": "label_state",
-#             "name": "test_min",
-#             "type": "min",
-#             "entity_id": "sensor.test_1",
-#             "unique_id": "very_unique_id",
-#         }
-#     }
+    test_label = label_registry.async_create(
+        "test",
+    )
 
-#     assert await async_setup_component(hass, "sensor", config)
-#     await hass.async_block_till_done()
+    sensor1_entity_entry = entity_registry.async_get_or_create(
+        "sensor", "test_1", "unique", suggested_object_id="test_1"
+    )
+    sensor1_entity_entry.labels.add(test_label.label_id)
+    assert sensor1_entity_entry.entity_id == "sensor.test_1"
 
-#     for value in VALUES_NUMERIC:
-#         hass.states.async_set(sensor_entity_entry.entity_id, value)
-#         await hass.async_block_till_done()
+    sensor2_entity_entry = entity_registry.async_get_or_create(
+        "sensor", "test_2", "unique", suggested_object_id="test_2"
+    )
+    sensor2_entity_entry.labels.add(test_label.label_id)
+    assert sensor2_entity_entry.entity_id == "sensor.test_2"
 
-#     state = hass.states.get("sensor.test_min")
+    config = MockConfigEntry(
+        domain="label_state",
+        data={},
+        options={
+            "name": "test_state",
+            "label": test_label.label_id,
+            "state_type": "state",
+            "state_to": "unavailable",
+        },
+        title="test_state",
+    )
 
-#     assert str(float(MIN_VALUE)) == state.state
+    await setup_integration(hass, config)
+    await hass.async_block_till_done()
 
-#     entity = entity_registry.async_get("sensor.test_min")
-#     assert entity.unique_id == "very_unique_id"
+    hass.states.async_set(sensor1_entity_entry.entity_id, state_1)
+    await hass.async_block_till_done()
 
+    hass.states.async_set(sensor2_entity_entry.entity_id, state_2)
+    await hass.async_block_till_done()
 
-# async def test_max_sensor(
-#     hass: HomeAssistant, entity_registry: er.EntityRegistry
-# ) -> None:
-#     """Test the max sensor."""
-#     sensor_entity_entry = entity_registry.async_get_or_create(
-#         "sensor", "test_1", "unique", suggested_object_id="test_1"
-#     )
-#     assert sensor_entity_entry.entity_id == "sensor.test_1"
+    state = hass.states.get("binary_sensor.test_state")
 
-#     config = {
-#         "sensor": {
-#             "platform": "label_state",
-#             "name": "test_max",
-#             "type": "max",
-#             "entity_id": "sensor.test_1",
-#             "unique_id": "very_unique_id",
-#         }
-#     }
-
-#     assert await async_setup_component(hass, "sensor", config)
-#     await hass.async_block_till_done()
-
-#     for value in VALUES_NUMERIC:
-#         hass.states.async_set(sensor_entity_entry.entity_id, value)
-#         await hass.async_block_till_done()
-
-#     state = hass.states.get("sensor.test_max")
-
-#     assert str(float(MAX_VALUE)) == state.state
-#     assert state.attributes.get(ATTR_STATE_CLASS) == SensorStateClass.MEASUREMENT
-
-#     entity = entity_registry.async_get("sensor.test_max")
-#     assert entity.unique_id == "very_unique_id"
+    assert state is not None
+    assert state.state == expected_state
 
 
-# async def test_value_error(
-#     hass: HomeAssistant, entity_registry: er.EntityRegistry
-# ) -> None:
-#     """Test value error."""
-#     sensor_entity_entry = entity_registry.async_get_or_create(
-#         "sensor", "test_1", "unique", suggested_object_id="test_1"
-#     )
-#     assert sensor_entity_entry.entity_id == "sensor.test_1"
+@pytest.mark.parametrize(
+    (
+        "state_1",
+        "state_2",
+        "state_lower_limit",
+        "state_upper_limit",
+        "expected_state",
+    ),
+    [
+        ("11", "12", "10", "20", "off"),
+        ("1", "12", "10", "20", "on"),
+    ],
+)
+async def test_numeric_state_sensor(
+    hass: HomeAssistant,
+    state_1: str,
+    state_2: str,
+    state_lower_limit: str,
+    state_upper_limit: str,
+    expected_state: str,
+    entity_registry: er.EntityRegistry,
+    label_registry: lr.LabelRegistry,
+) -> None:
+    """Test the state sensor."""
 
-#     config = {
-#         "sensor": {
-#             "platform": "label_state",
-#             "name": "test_max",
-#             "type": "max",
-#             "entity_id": "sensor.test_1",
-#             "unique_id": "very_unique_id",
-#         }
-#     }
+    test_label = label_registry.async_create(
+        "test",
+    )
 
-#     assert await async_setup_component(hass, "sensor", config)
-#     await hass.async_block_till_done()
+    sensor1_entity_entry = entity_registry.async_get_or_create(
+        "sensor", "test_1", "unique", suggested_object_id="test_1"
+    )
+    sensor1_entity_entry.labels.add(test_label.label_id)
+    assert sensor1_entity_entry.entity_id == "sensor.test_1"
+    assert test_label.label_id in sensor1_entity_entry.labels
 
-#     for value in VALUES_ERROR:
-#         hass.states.async_set(sensor_entity_entry.entity_id, value)
-#         await hass.async_block_till_done()
+    sensor2_entity_entry = entity_registry.async_get_or_create(
+        "sensor", "test_2", "unique", suggested_object_id="test_2"
+    )
+    sensor2_entity_entry.labels.add(test_label.label_id)
+    assert sensor2_entity_entry.entity_id == "sensor.test_2"
 
-#     state = hass.states.get("sensor.test_max")
+    config = MockConfigEntry(
+        domain="label_state",
+        data={},
+        options={
+            "name": "test_numeric_state",
+            "label": test_label.label_id,
+            "state_type": "numeric_state",
+            "state_lower_limit": state_lower_limit,
+            "state_upper_limit": state_upper_limit,
+        },
+        title="test_numeric_state",
+    )
 
-#     assert state.attributes.get(ATTR_STATE_CLASS) == SensorStateClass.MEASUREMENT
+    config.add_to_hass(hass)
+    # await setup_integration(hass, config)
+    assert await hass.config_entries.async_setup(config.entry_id)
+    await hass.async_block_till_done()
 
-#     entity = entity_registry.async_get("sensor.test_max")
-#     assert entity.unique_id == "very_unique_id"
+    hass.states.async_set(sensor1_entity_entry.entity_id, state_1)
+    await hass.async_block_till_done()
+
+    hass.states.async_set(sensor2_entity_entry.entity_id, state_2)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("binary_sensor.test_numeric_state")
+
+    assert state is not None
+    assert state.state == expected_state
